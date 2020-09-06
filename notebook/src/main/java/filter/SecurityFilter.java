c907a6cd5c295327a1512c9199b49332409e0915
nhathung <nhathungqs@gmail.com>
2020-09-06 10:44:21 +0700

    Add filter feature

---
+++ notebook/src/main/java/filter/SecurityFilter.java
@@ -0,0 +1,43 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = "/home")
+public class SecurityFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		String servletPath = req.getServletPath();
+	    if (req.getParameter("id") == null || req.getParameter("kind")==null) {
+	    	resp.sendRedirect(req.getContextPath() + "/login");
+	    	return;
+	    }
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
